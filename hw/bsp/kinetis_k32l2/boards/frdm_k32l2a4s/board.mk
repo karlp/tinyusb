d6eaefba699283cf19a71efca087e9cbda364d24
Karl Palsson <karlp@tweak.au>
2024-02-13 10:53:52 +0000

    bsp: kinetis: frdm-k32l2a4s: fix USB clock
    Correctly ensure that the div1 firc48 clock output is enabled.
    
    Fixes: https://github.com/hathach/tinyusb/issues/2464
    Signed-off-by: Karl Palsson <karl.palsson@marel.com>

--- hw/bsp/kinetis_k32l2/boards/frdm_k32l2a4s/board.mk
+++ hw/bsp/kinetis_k32l2/boards/frdm_k32l2a4s/board.mk
@@ -8,8 +8,8 @@ CFLAGS += -Wno-error=unused-parameter -Wno-error=redundant-decls -Wno-error=cast
 # All source paths should be relative to the top level.
 LD_FILE = $(MCU_DIR)/gcc/K32L2A41xxxxA_flash.ld
 
-SRC_C += \
-	$(MCU_DIR)/project_template/clock_config.c \
+#SRC_C += $(SDK_DIR)/boards/frdmk32l2a4s/project_template/clock_config.c  # this needs SMC, and runs at 96Mhz...
+SRC_C += $(SDK_DIR)/boards/frdmk32l2a4s/clock_config.c
 
 # For flash-jlink target
 JLINK_DEVICE = K32L2A41xxxxA
